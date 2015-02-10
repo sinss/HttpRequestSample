08d18efdb4952a7d5ed3a3fc0c5e33d5e0ad024a
Leo Chang <sinss0000@gmail.com>
2015-02-10 23:49:18 +0800

    Initial Commit

---
+++ HttpRequestSample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HttpRequestSample
+//
+//  Created by Leo on 2/10/15.
+//  Copyright (c) 2015 Perfectidea Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HttpRequestSample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HttpRequestSample
+//
+//  Created by Leo on 2/10/15.
+//  Copyright (c) 2015 Perfectidea Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
